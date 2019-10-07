54efa6ff1cbb22a2dee6123feee6d756846c4e1a
folkaras <efolkaras@gmail.com>
2019-10-07 14:06:48 +0300

    Updates

---
+++ precache-manifest.82234fc166c44b202dfd0bf801a1b3aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cb70441fc372897dc5ecb2e7c48556",
+    "url": "/skytrack_test/index.html"
+  },
+  {
+    "revision": "876e4a4479f24e56f42d",
+    "url": "/skytrack_test/static/css/main.4322c0b8.chunk.css"
+  },
+  {
+    "revision": "45c530bdc07c29d3f1ee",
+    "url": "/skytrack_test/static/js/2.da9a46e7.chunk.js"
+  },
+  {
+    "revision": "876e4a4479f24e56f42d",
+    "url": "/skytrack_test/static/js/main.499b7d8f.chunk.js"
+  },
+  {
+    "revision": "9d948db62ad77819836b",
+    "url": "/skytrack_test/static/js/runtime-main.77f13953.js"
+  }
+]);
