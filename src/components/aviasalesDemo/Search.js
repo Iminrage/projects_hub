9fe137b02fd4af9fb7cad04c79fcd733f7583aa8
folkaras <efolkaras@gmail.com>
2019-10-07 14:51:53 +0300

    moved styles to bottom

--- src/components/aviasalesDemo/Search.js
+++ src/components/aviasalesDemo/Search.js
@@ -2,85 +2,7 @@ import React from "react";
 import SelectType from "./selectType";
 import styled from "styled-components";
 
-const MainSearchInputs = styled.input`
-  border: none;
-  box-shadow: none;
-  box-sizing: border-box;
-  padding: 18px 16px;
-  margin: 0 1px;
-  height: 56px;
-  width: ${props => (props.type === "date" ? "180px" : "224px")};
-  border-radius: ${props => (props.dataset === 0 ? "6px 0 0 6px" : null)};
-  background: #ffffff;
 
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 16px;
-  line-height: 20px;
-  color: #4a4a4a;
-`;
-
-const InputsWrapper = styled.div`
-  display: flex;
-`;
-const Form = styled.form`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-content: center;
-  align-items: center;
-`;
-const SearchButton = styled.button`
-  margin-top: 48px;
-  box-shadow: none;
-  border: none;
-  padding: 15px 74px 16px 45px;
-  background: #ff9241;
-  border-radius: 4px;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: 900;
-  font-size: 28px;
-  line-height: 33px;
-  color: #ffffff;
-  flex-grow: 0;
-  transition: all 0.1s;
-  cursor: pointer;
-  :hover {
-    transform: scale(1.02);
-    box-shadow: 0px 8px 25px 0 rgba(74, 74, 74, 0.22);
-  }
-  :active {
-    background: #ff9a00;
-  }
-`;
-const Swap = styled.div`
-  position: absolute;
-  width: 16px;
-  height: 16px;
-  border: 1px solid red;
-  top: calc(50% - 8px);
-  right: -9px;
-  border-radius: 15px;
-  transform-origin: 50% 50%;
-  cursor: pointer;
-  transition: all 0.2s;
-  :hover {
-    transform: rotate(-45deg);
-  }
-`;
-const MainInputWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-`;
-const CityCode = styled.span`
-  position: absolute;
-  color: grey;
-  left: 0;
-`;
 class Search extends React.Component {
   constructor(props) {
     super(props);
@@ -163,5 +85,85 @@ class Search extends React.Component {
     );
   }
 }
+
+const MainSearchInputs = styled.input`
+  border: none;
+  box-shadow: none;
+  box-sizing: border-box;
+  padding: 18px 16px;
+  margin: 0 1px;
+  height: 56px;
+  width: ${props => (props.type === "date" ? "180px" : "224px")};
+  border-radius: ${props => (props.dataset === 0 ? "6px 0 0 6px" : null)};
+  background: #ffffff;
+
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  color: #4a4a4a;
+`;
+
+const InputsWrapper = styled.div`
+  display: flex;
+`;
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+`;
+const SearchButton = styled.button`
+  margin-top: 48px;
+  box-shadow: none;
+  border: none;
+  padding: 15px 74px 16px 45px;
+  background: #ff9241;
+  border-radius: 4px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 28px;
+  line-height: 33px;
+  color: #ffffff;
+  flex-grow: 0;
+  transition: all 0.1s;
+  cursor: pointer;
+  :hover {
+    transform: scale(1.02);
+    box-shadow: 0px 8px 25px 0 rgba(74, 74, 74, 0.22);
+  }
+  :active {
+    background: #ff9a00;
+  }
+`;
+const Swap = styled.div`
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  border: 1px solid red;
+  top: calc(50% - 8px);
+  right: -9px;
+  border-radius: 15px;
+  transform-origin: 50% 50%;
+  cursor: pointer;
+  transition: all 0.2s;
+  :hover {
+    transform: rotate(-45deg);
+  }
+`;
+const MainInputWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+`;
+const CityCode = styled.span`
+  position: absolute;
+  color: grey;
+  left: 0;
+`;
 
 export default Search;
