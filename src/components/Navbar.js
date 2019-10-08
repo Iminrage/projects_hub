69dbfc7903219b555d465ef26f572c466f6b6272
folkaras <efolkaras@gmail.com>
2019-10-08 18:18:27 +0300

    new navbar bg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import navBg from "./img/nav_bg.jpg";
+import navBg from "./img/nav_bg.png";
 
 function Navbar() {
   return (
@@ -9,10 +9,15 @@ function Navbar() {
       <Nav>
         <Ul>
           <Li>
+            <Alink href="https://github.com/Iminrage/projects_hub">GitHub</Alink>
+          </Li>
+          <Li>
             <NewNavLink to="/projects_hub/">Home</NewNavLink>
           </Li>
-					<Li>
-            <NewNavLink to="/projects_hub/aviasales_demo/">Aviasales Demo</NewNavLink>
+          <Li>
+            <NewNavLink to="/projects_hub/aviasales_demo/">
+              Aviasales Demo
+            </NewNavLink>
           </Li>
           <Li>
             <NewNavLink to="/projects_hub/articles/">Article Home</NewNavLink>
@@ -27,9 +32,8 @@ function Navbar() {
 }
 
 const Nav = styled.nav`
-  border-bottom: 1px solid #c6e9ff;
-	padding: 10px 150px;
-	background: url(${navBg}) 0 50% / cover no-repeat;
+  padding: 10px 150px;
+  background: #5a7c86;
 `;
 const Ul = styled.ul`
   margin: 0;
@@ -45,12 +49,24 @@ const NewNavLink = styled(NavLink)`
   padding: 10px 40px 10px 30px;
   border: 1px solid gray;
   text-decoration: none;
-	color: white;
-	background-color: rgba(58,58,58, 0.5);
-	:hover{
-		background-color: rgba(58,58,58, 0.8);
-		color: #cbebff;
-	}
+  color: white;
+  background-color: rgba(58, 58, 58, 0.5);
+  :hover {
+    background-color: rgba(58, 58, 58, 0.8);
+    color: #cbebff;
+  }
+`;
+const Alink = styled.a`
+display: inline-block;
+  padding: 10px 40px 10px 30px;
+  border: 1px solid gray;
+  text-decoration: none;
+  color: white;
+  background-color: rgba(58, 58, 58, 0.5);
+  :hover {
+    background-color: rgba(58, 58, 58, 0.8);
+    color: #cbebff;
+  }
 `;
 
 export default Navbar;
