e94c8cfd329210d540fb9b34b140033a88086f68
folkaras <efolkaras@gmail.com>
2019-10-08 17:39:18 +0300

    moved styles to bottom, fixed container

--- src/components/articles/ArticleHome.js
+++ src/components/articles/ArticleHome.js
@@ -3,29 +3,6 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const PostLinkWrapper = styled.div`
-  margin-bottom: 30px;
-`;
-const PostHeading = styled.h2`
-  padding: 5px 100px 10px 150px;
-  color: black;
-`;
-const NewLink = styled(Link)`
-  display: flex;
-  border: 1px solid grey;
-  text-decoration: none;
-`;
-const PostText = styled.p`
-  padding: 0 100px 20px 100px;
-  color: black;
-`;
-const TextContent = styled.div``;
-const ExtraContent = styled.div``;
-const PostsWrapper = styled.div`
-  padding: 0 70px;
-  margin: 40px 0;
-`;
-
 function Home(props) {
   console.log(props);
   const { posts } = props;
@@ -54,5 +31,29 @@ const mapStateToProps = state => {
     posts: state.posts
   };
 };
+
+const PostLinkWrapper = styled.div`
+
+  margin-bottom: 30px;
+`;
+const PostHeading = styled.h2`
+  padding: 5px 100px 10px 150px;
+  color: black;
+`;
+const NewLink = styled(Link)`
+  display: flex;
+  border: 1px solid grey;
+  text-decoration: none;
+`;
+const PostText = styled.p`
+  padding: 0 100px 20px 100px;
+  color: black;
+`;
+const TextContent = styled.div``;
+const ExtraContent = styled.div``;
+const PostsWrapper = styled.div`
+  width: 960px;
+  margin: 40px auto;
+`;
 
 export default connect(mapStateToProps)(Home);
