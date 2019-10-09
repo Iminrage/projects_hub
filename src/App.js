b43f95ce3fc4769de148f3cfc45e533b92eb58ef
folkaras <efolkaras@gmail.com>
2019-10-09 15:02:18 +0300

    moved navbar functions to navbar component

--- src/App.js
+++ src/App.js
@@ -1,71 +1,32 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Article from "./components/articles/Article";
 import ArticleHome from "./components/articles/ArticleHome";
 import Navbar from "./components/Navbar";
 import Layout from "./components/layout/Layout";
 import Home from "./components/Home";
 import AviasalesApp from "./components/aviasalesDemo/AviasalesApp";
-import Inprgrs from "./components/img/inprogress.png";
 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar></Navbar>
 
-class App extends Component {
-  state = {
-    isOpen: false
-  };
-  toggleNav = () => {
-    console.log(this.state.isOpen);
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects_hub/articles/" component={ArticleHome} />
+        <Route exact path="/projects_hub/" component={Home} />
+        <Route
+          exact
+          path="/projects_hub/aviasales_demo/"
+          component={AviasalesApp}
+        />
+        <Route path="/projects_hub/layout" component={Layout} />
+        <Route path="/projects_hub/:post_id" component={Article} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-    this.setState({ isOpen: !this.state.isOpen });
-  };
-  render() {
-    return (
-      <BrowserRouter>
-				
-        <NavCase state={this.state.isOpen}>
-          <Navbar></Navbar>
-					<Plate src={Inprgrs} alt="in progress" width="256" height="113"></Plate>
-        </NavCase>
-        <NavToggle onClick={this.toggleNav}>{this.state.isOpen? "Hide navigation": "Show navigation"}</NavToggle>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route
-            exact
-            path="/projects_hub/articles/"
-            component={ArticleHome}
-          />
-          <Route exact path="/projects_hub/" component={Home} />
-          <Route
-            exact
-            path="/projects_hub/aviasales_demo/"
-            component={AviasalesApp}
-          />
-          <Route path="/projects_hub/layout" component={Layout} />
-          <Route path="/projects_hub/:post_id" component={Article} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
-
-const NavCase = styled.div`
-  display: ${props => (props.state == 1 ? "block" : "none")};
-`;
-const Plate = styled.img`
-position: absolute;
-top: 100px;
-opacity: 0.5;
-`;
-const NavToggle = styled.button`
-  position: fixed;
-  top: 100px;
-	right: 100px;
-	padding: 5px;
-	margin: 0;
-	color: lightgrey;
-	border: 1px solid grey;
-	box-shadow: 10px 10px 25px 0px rgba(0,0,0,0.75);
-	background: #50757e;
-`;
 export default App;
