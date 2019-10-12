74cfac68929db56a5853790a47bca53361107843
folkaras <efolkaras@gmail.com>
2019-10-12 11:40:20 +0300

    added hint

--- src/components/articles/ArticleHome.js
+++ src/components/articles/ArticleHome.js
@@ -23,7 +23,21 @@ function Home(props) {
   ) : (
     <div className="">No posts yet</div>
   );
-  return <PostsWrapper>{postsList}</PostsWrapper>;
+  return (
+    <div className="">
+      <PostsWrapper>{postsList}</PostsWrapper>
+      <Hint>
+        <HintText>
+          Данный небольшой проект был сделан в рамках тестового задания для
+          компании SKYTRACK. Задание было следующим: 
+					приложение представляет из себя список заголовков
+          статей, при клике на статью должна открываться сама статья (react-router) для чтения, при клике на кнопку «Открыть комментарий»
+          должен открываться список всех комментариев этой статьи. <br/>
+          <HintBText>Проекты находятся на стадии доработки</HintBText>
+        </HintText>
+      </Hint>
+    </div>
+  );
 }
 
 const mapStateToProps = state => {
@@ -33,7 +47,6 @@ const mapStateToProps = state => {
 };
 
 const PostLinkWrapper = styled.div`
-
   margin-bottom: 30px;
 `;
 const PostHeading = styled.h2`
@@ -54,7 +67,28 @@ const ExtraContent = styled.div``;
 const PostsWrapper = styled.div`
   width: 960px;
   margin: 0 auto;
-	padding: 40px 0;
+  padding: 40px 0;
+`;
+
+const Hint = styled.div`
+  position: absolute;
+  top: 80%;
+  left: 0;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+const HintText = styled.p`
+  color: #dcf1ff;
+  text-align: center;
+  width: 30%;
+  padding: 20px 0;
+  margin: 0 auto;
+`;
+const HintBText = styled.b`
+  color: brown;
+  display: inline-block;
+  padding-top: 10px;
+  text-transform: uppercase;
 `;
 
 export default connect(mapStateToProps)(Home);
