89b492c08eb2b6bb640a5de03ded006356e4e70c
folkaras <efolkaras@gmail.com>
2019-10-29 16:04:25 +0300

    moved hints components to HOC

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import styled, { keyframes } from "styled-components";
 import LinksPop from "./LinksPop";
+import Hint from "./Hint"
 import Bg from "../img/bg.jpg";
-import InfoIcon from "../img/infoIcon.png";
 
 class Home extends Component {
   state = {
@@ -154,20 +154,8 @@ class Home extends Component {
             </PaginationBtn>
           </Pagination>
         </About>
-        <Hint opened={this.state.hintIsOpen}>
-          <HintText>
-            Добро пожаловать в центр моих проектов!
-            <br />
-            Спасибо, что заглянули. Для перемещения по проектам откройте меню
-            навигации в правом верхнем углу и с помощью стрелок выберите
-            интересующий проект, и просто нажмите на ссылку.
-            <br />
-            <HintBText>Проекты находятся на стадии доработки</HintBText>
-          </HintText>
-        </Hint>
-        <HintToggle
-          onClick={() => this.setState({ hintIsOpen: !this.state.hintIsOpen })}
-        />
+        <Hint></Hint>
+        
         <LinksPop></LinksPop>
       </HomePageContainer>
     );
@@ -181,15 +169,7 @@ const widthGrow = keyframes`
 		transform: scale3d(1, 1, 1);
 	}
 `;
-const fadeInUp = keyframes`
-  from {
-    opacity: 0;
-    transform: translate3d(0, 5%, 0);
-  }
-  to {
-    transform: translate3d(0, 0, 0);
-  }
-`;
+
 const fadeIn = keyframes`
 	from{
 		opacity: 0;
@@ -303,63 +283,7 @@ const Dot = styled(PaginationBtn)`
   width: 10px;
   height: 10px;
 `;
-const Hint = styled.div`
-  display: ${props => (props.opened ? "block" : "none")};
-  position: absolute;
-  bottom: 10%;
-  left: 0;
-  width: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  transform-origin: left;
-  animation: ${fadeInUp} 0.5s both;
-	@media (max-width: 640px) {
-		height: 100%;
-		top: 0;
-		left: 0;
-		right: auto;
-		bottom: auto;
-		background-color: rgba(0, 0, 0, 0.95);
-		animation: ${fadeIn} 0.5s both;
-		display: ${props => (props.opened ? "flex" : "none")};
-		align-items: center;
-  }
-`;
-const HintToggle = styled.button`
-  position: absolute;
-  bottom: 30px;
-  right: 30px;
-  width: 40px;
-  height: 40px;
-	border: none;
-	background: url(${InfoIcon}) 0 0 / contain no-repeat;
-	cursor: pointer;
-	@media (max-width: 640px) {
-		top: 10px;
-		left: 10px;
-		right: auto;
-		bottom: auto;
-  }
-`;
-const HintText = styled.p`
-  color: #dcf1ff;
-  text-align: center;
-	padding: 20px 200px;
-	margin: 0;
-	@media (max-width: 1500px) {
-		padding: 20px 400px;
-  }
-	@media (max-width: 1200px) {
-		padding: 10px 200px;
-  }
-	@media (max-width: 1000px) {
-		padding: 10px 50px;
-  }
-`;
-const HintBText = styled.b`
-  color: brown;
-  display: inline-block;
-  padding-top: 10px;
-  text-transform: uppercase;
-`;
+
+
 
 export default Home;
