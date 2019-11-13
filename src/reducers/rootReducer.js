242a81aa44b0e703923d732f5353d60ad94b7587
folkaras <efolkaras@gmail.com>
2019-11-13 21:14:39 +0300

    del aviaInput reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
-import aviaInputsReducer from "./aviaInputsReducer";
 import commentsReducer from "./commentsReducer";
 import postReducer from "./postReducer";
 
@@ -9,7 +8,6 @@ const rootReducer = combineReducers({
 	form: formReducer,
 	posts: postReducer,
 	comments: commentsReducer,
-  aviaInputs: aviaInputsReducer
 });
 
 export default rootReducer;
