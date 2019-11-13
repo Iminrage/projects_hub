95b21019676884f6361d04de16297cadb5555d56
folkaras <efolkaras@gmail.com>
2019-11-13 21:12:54 +0300

    created component

---
+++ src/components/aviasalesDemo/swap.js
@@ -0,0 +1,35 @@
+<Swap onClick={this.handleSwap}>
+  <SwapCircle></SwapCircle>
+  <SwapArrowL>
+    <svg
+      width="16"
+      height="8"
+      viewBox="0 0 16 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M5 8L0 4L5 0V3H16V5H5V8Z"
+        fill="#1BA6D2"
+      />
+    </svg>
+  </SwapArrowL>
+  <SwapArrowR>
+    <svg
+      width="16"
+      height="8"
+      viewBox="0 0 16 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M11 0L16 4L11 8V5L0 5V3L11 3V0Z"
+        fill="#1BA6D2"
+      />
+    </svg>
+  </SwapArrowR>
+</Swap>;
